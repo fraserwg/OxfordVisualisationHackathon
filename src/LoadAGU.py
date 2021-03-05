6887b3c3821f89df8378cee84135e84067cb2323
Fraser Goldsworth <45496050+fraserwg@users.noreply.github.com>
2021-03-05 15:57:33 +0000

    Update LoadAGU.py

--- src/LoadAGU.py
+++ src/LoadAGU.py
@@ -5,3 +5,13 @@ data_path = '../data/ProcessedAGU.csv'
 
 # Read in using pandas
 df = pd.read_csv(data_path)
+
+
+# Read in using csv module and convert to lists
+with open('../data/ProcessedAGU.csv') as file:
+    csv_file = csv.reader(file)
+    data_set = list()
+    for line in csv_file:
+        data_set.append(line)
+
+N, country, state, city, lon, lat, distance = list(zip(*data_set))
