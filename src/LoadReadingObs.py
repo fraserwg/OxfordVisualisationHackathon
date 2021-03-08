40c74d6233509856d768159d58a5ca605275aedf
bethdingley <47028564+bethdingley@users.noreply.github.com>
2021-03-08 16:49:05 +0000

    Initial update on loading in Reading data

--- src/LoadReadingObs.py
+++ src/LoadReadingObs.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
+import pandas as pd
 
+data_path = '../data/reading_data.csv'
+
+# Read in using pandas
+df = pd.read_csv(data_path)
+
+
+# Read in using csv module and convert to lists
+with open('../data/reading_data.csv') as file:
+    csv_file = csv.reader(file)
+    data_set = list()
+    for line in csv_file:
+        data_set.append(line)
+
+N, country, state, city, lon, lat, distance = list(zip(*data_set))
